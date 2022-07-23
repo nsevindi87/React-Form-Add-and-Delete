e4feba4b59f413a5bce272f74f4e944bb3b13d66
Nizami Sevindi <nsevindi87@gmail.com>
2022-07-23 22:27:07 +0100

    Bos react calisma sayfasi olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import {useState} from 'react';
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+
 
 function App() {
-  const [todos, setTodos] = useState(["Berkant", "Berkant2"])
-
   return (
-    <>
-    <ToastContainer />
-    <TodoForm setTodos={setTodos} todos={todos}/>
-    <TodoList todos={todos}/>
-    </>
+    <h1>Hello world</h1>
   );
 }
 
