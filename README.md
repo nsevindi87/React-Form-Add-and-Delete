e8e637c4784f0e0ede419ad31f9569ea92d1958a
Nizami Sevindi <nsevindi87@gmail.com>
2022-09-18 17:29:09 +0200

    practice

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+Fontawesome ve Boostrap ekli bir proje.
+
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Component } from "react";
 
+class App extends Component{
+  render(){
+    const title = "User App"
 
-function App() {
-  return (
-    <h1>Hello world</h1>
-  );
+    return (
+      <div className="container">
+        <h5>{title}</h5>
+        <label htmlFor="name">Name</label>
+        <p className="lead">This is sample text</p>
+      </div>
+    )
+  }
 }
 
 export default App;
